f87ea3a0ec00d0a804a722decee48b5807dbe781
OllyGilbert <olly.gilbert@gmail.com>
2014-12-08 10:25:06 +0000

    comments printing to screen inline with stories

--- jrm_app/app/helpers/stories_helper.rb
+++ jrm_app/app/helpers/stories_helper.rb
@@ -1,2 +1,10 @@
 module StoriesHelper
+
+  def grab_data_for_journalist(journalist)
+    all_instances = []
+    all_instances << journalist.stories
+    all_instances << journalist.comments
+    all_instances.flatten!
+    all_instances.sort_by{ |instance| instance.created_at}.reverse
+  end
 end
