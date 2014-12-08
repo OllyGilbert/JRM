6f657f8ad254cb31a5d97406da4f09a1c9208b29
OllyGilbert <olly.gilbert@gmail.com>
2014-12-08 17:00:41 +0000

    twitter widget width reduced

--- jrm_app/db/seeds.rb
+++ jrm_app/db/seeds.rb
@@ -8,9 +8,9 @@
 
 Journo1 = Journalist.create(first_name: 'Anna', last_name: 'Hodgekiss', title: 'correspondent', media_outlet: 'mailonline', twitter: 'annahodgekiss')
 Journo2 = Journalist.create(first_name: 'Lucy', last_name: 'Thackray', title: 'correspondent', media_outlet: 'mailonline', twitter: 'LucyThack')
-Journo3 = Journalist.create(first_name: 'Matt', last_name: 'Chorley', title: 'correspondent', media_outlet: 'mailonline', twitter: '@MattChorley')
-Journo4 = Journalist.create(first_name: 'Katie', last_name: 'Allen', title: 'correspondent', media_outlet: 'guardian', twitter: '@KatieAllenGdn')
-Journo5 = Journalist.create(first_name: 'Ian', last_name: 'Black', title: 'correspondent', media_outlet: 'guardian', twitter: '@ian_black')
+Journo3 = Journalist.create(first_name: 'Matt', last_name: 'Chorley', title: 'correspondent', media_outlet: 'mailonline', twitter: 'MattChorley')
+Journo4 = Journalist.create(first_name: 'Katie', last_name: 'Allen', title: 'correspondent', media_outlet: 'guardian', twitter: 'KatieAllenGdn')
+Journo5 = Journalist.create(first_name: 'Ian', last_name: 'Black', title: 'correspondent', media_outlet: 'guardian', twitter: 'ian_black')
 
 Position1 = Position.create(title: "Health Editor", publisher_id: 1, journalist_id: 1)
 Position2 = Position.create(title: "Correspondent", publisher_id: 1, journalist_id: 2)
